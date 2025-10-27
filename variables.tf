30347de0be39cb6a4fbab54da1c439e205096074
Mulka sandhya rani <sandhyaranimulka11@gmail.com>
2025-10-27 12:10:45 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_port" {
+  description = "Local port for AWS simulated server"
+  default     = 8081
+}
+
+variable "gcp_port" {
+  description = "Local port for GCP simulated server"
+  default     = 8082
+}
